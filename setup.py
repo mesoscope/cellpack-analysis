419c57d13020cc78717896a96318ee8504c367bd
Saurabh Mogre <saurabh.mogre@alleninstitute.org>
2024-02-20 14:49:25 -0800

    Update dependencies and exclude directories in flake8 configuration

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
     keywords="cellpack_analysis",
     name="cellpack_analysis",
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
-    python_requires=">=3.8",
+    python_requires=">=3.9,<3.12",
     setup_requires=setup_requirements,
     test_suite="aicscytoparam/tests",
     tests_require=test_requirements,
